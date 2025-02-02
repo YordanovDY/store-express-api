1c4e0586047e119678865f78a0a5e13fab683f5a
YordanovDY <yordanovdy23@gmail.com>
2025-02-02 17:46:09 +0200

    Add status to response on register

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,23 +6,23 @@ const authController = Router();
 authController.post('/register', async (req, res) => {
     const { email, password } = req.body;
 
-    if(!email || !password){
-        return res.status(400).json({ message: 'Email and password are required.' });
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required', status: 400 });
     }
 
     try {
         await authService.register(email, password);
 
-        res.status(201).json({ message: 'User created.' });
+        res.status(201).json({ message: 'User created', status: 201 });
 
     } catch (err) {
-        if(err.message === 'User already exists'){
-            console.error("Registration error:", err.message); 
-            return res.status(409).json({ message: err.message });
+        if (err.message === 'User already exists') {
+            console.error("Registration error:", err.message);
+            return res.status(409).json({ message: err.message, status: 409 });
         }
 
-        console.error("Registration error:", err.message); 
-        res.status(500).json({ message: 'Internal server error.' }); 
+        console.error("Registration error:", err.message);
+        res.status(500).json({ message: 'Internal server error', status: 500 });
     }
 });
 
