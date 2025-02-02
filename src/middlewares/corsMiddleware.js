83cf4a9fc0e7ba25bc93b625b7cc5bed9ffe4a06
YordanovDY <yordanovdy23@gmail.com>
2025-02-02 11:50:59 +0200

    Add options to the CORS middleware

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,6 +1,13 @@
-export default function allowCORSRequests() {
+export default function allowCORSRequests(options = {}) {
     return (req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        if (Object.keys(options).length === 0) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            return next();
+        }
+
+        for (const key in options) {
+            res.setHeader(key, options[key]);
+        }
 
         next();
     }
