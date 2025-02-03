8843126b9abe43b71b8fe8ab8c87580e45ad5fc8
YordanovDY <yordanovdy23@gmail.com>
2025-02-03 12:23:35 +0200

    Create working login endpoint

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -26,4 +26,35 @@ authController.post('/register', async (req, res) => {
     }
 });
 
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required', status: 400 });
+    }
+
+    try {
+        const userData = await authService.login(email, password);
+        const { token, id, role } = userData;
+        const user = {
+            id,
+            email: userData.email,
+            role
+        };
+
+        res.status(200)
+            .cookie('Auth', token, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+            .json({ message: 'Successful login', status: 200, result: { user } });
+
+    } catch (err) {
+        if (err.message === 'Invalid user or password') {
+            console.error("Registration error:", err.message);
+            return res.status(401).json({ message: err.message, status: 401 });
+        }
+
+        console.error("Registration error:", err.message);
+        res.status(500).json({ message: 'Internal server error', status: 500 });
+    }
+});
+
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,9 +1,14 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from "../models/User.js";
 import { ROLES } from '../config/constants.js';
+import 'dotenv/config';
+
+const { JWT_SECRET } = process.env;
 
 const authService = {
     register,
+    login,
 };
 
 async function register(email, password) {
@@ -16,6 +21,31 @@ async function register(email, password) {
     return User.create({ email, password, role: ROLES.Customer });
 }
 
+async function login(email, password) {
+    const foundUser = await findUser(email);
+
+    if (!foundUser) {
+        throw new Error('Invalid user or password');
+    }
+
+    const isValidPassword = await bcrypt.compare(password, foundUser.password);
+
+    if (!isValidPassword) {
+        throw new Error('Invalid user or password');
+    }
+
+    const payload = {
+        id: foundUser.id,
+        email: foundUser.email,
+        role: foundUser.role,
+    }
+
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' });
+
+    return {
+        ...payload,
+        token
+    };
 }
 
 async function findUser(email) {
